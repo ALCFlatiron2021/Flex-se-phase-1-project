4dc9025aed9f0a1b43100f2b86a089d786b5ab2f
Alan Cinsavich <alan.cimsavich@gmail.com>
2022-03-14 15:15:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+//Remeber three event listeners required
+//First can be a json.db sever load aka addeventlisenter DOM content loaded
+//The other event listeners will be:
+//  Drop down menu: "On change event"
+// Like button "click event"
